eede5a1b90c3c64edfc303801a8c6be6fad3d673
h0ttab <myemail@example.com>
2026-07-07 22:35:45 +0300

    docs: add openapi docs

--- src/main/java/com/app/filmorate/FilmorateApplication.java
+++ src/main/java/com/app/filmorate/FilmorateApplication.java
@@ -1,9 +1,16 @@
 package com.app.filmorate;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+        title = "Filmorate API",
+        version = "1.0",
+        description = "API documentation for the Filmorate movie rating and recommendation service"
+))
 public class FilmorateApplication {
     public static void main(String[] args) {
         SpringApplication.run(FilmorateApplication.class, args);
